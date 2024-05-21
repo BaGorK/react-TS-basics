3b9a80d3729f15d6e1d47dbee118f5ea4e9dc56b
BaGorK <edmealemkassahun@gmail.com>
2024-05-21 05:45:04 +0300

    using useRef with typescript

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,21 +1,25 @@
-import { type FormEvent } from 'react';
+import { useRef, type FormEvent } from 'react';
 
 export default function NewGoal() {
+  const goalRef = useRef<HTMLInputElement>(null);
+  const summaryRef = useRef<HTMLInputElement>(null);
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    new FormData(e.currentTarget);
+    const enteredGoal = goalRef.current!.value;
+    const enteredSummary = summaryRef.current!.value;
   };
 
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
       <p>
         <label htmlFor='goal'>Your goal</label>
-        <input type='text' id='goal' />
+        <input ref={goalRef} type='text' id='goal' />
       </p>
       <p>
         <label htmlFor='summary'>Short summary</label>
-        <input type='text' id='summary' />
+        <input ref={summaryRef} type='text' id='summary' />
       </p>
       <p>
         <button>Add Goal</button>
@@ -23,3 +27,5 @@ export default function NewGoal() {
     </form>
   );
 }
+
+// useRef contains undefined as a default value // undefined means that their is no value at all // null means their may no value under certain conditions
