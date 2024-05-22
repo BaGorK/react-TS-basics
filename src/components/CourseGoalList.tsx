c8735ec38a9cebe40e68e84c7ffc36d5e2af0aff
BaGorK <edmealemkassahun@gmail.com>
2024-05-23 02:23:22 +0300

    feat: add a warningBox | hint on courseGoalList

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from './CourseGoal';
 import { CourseGoal as CourseGoalType } from '../App';
+import InfoBox from './InfoBox';
+import { ReactNode } from 'react';
 
 type CourseGoalListProps = {
   goals: CourseGoalType[];
@@ -10,15 +12,36 @@ export default function CourseGoalList({
   goals,
   onDelete,
 }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode='hint'>
+        You have no course goals yet. Start adding some
+      </InfoBox>
+    );
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode='warning'>
+        You are collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDelete}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDelete}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
