9fb4e6aaf4b5d735e08f615bf2ed4b2e3ce3c39b
BaGorK <edmealemkassahun@gmail.com>
2024-05-23 21:53:48 +0300

    feat: building components with discriminated unions

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,12 +1,21 @@
 import { ReactNode } from 'react';
 
-type InfoBoxProps = {
-  mode: 'hint' | 'warning';
-  severity?: 'low' | 'medium' | 'high';
+type HintBoxProps = {
+  mode: 'hint';
   children: ReactNode;
 };
 
-export default function InfoBox({ mode, children, severity }: InfoBoxProps) {
+type WarningBoxProps = {
+  mode: 'warning';
+  severity: 'low' | 'medium' | 'high';
+  children: ReactNode;
+};
+
+type InfoBoxProps = HintBoxProps | WarningBoxProps;
+
+export default function InfoBox(props: InfoBoxProps) {
+  const { mode, children } = props; // we can not destructure severity here | b/c it may not be available in hint mode
+
   if (mode === 'hint') {
     return (
       <aside className='infobox infobox-hint'>
@@ -15,8 +24,14 @@ export default function InfoBox({ mode, children, severity }: InfoBoxProps) {
     );
   }
 
+  const { severity } = props; // if we pass the above condition we must be in warning mode so we can destructure severity here with out getting errors
+
   return (
-    <aside className={`infobox infobox-warning warning--${severity}`}>
+    <aside
+      className={`infobox infobox-warning ${
+        severity ? `warning--${severity}` : ''
+      }`}
+    >
       <h2>Warning</h2>
       <p>{children}</p>
     </aside>
